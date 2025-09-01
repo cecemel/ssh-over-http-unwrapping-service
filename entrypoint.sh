6c64a51037b2c51271f69121a652f9df4234d642
felix <ruiz.felixruiz@gmail.com>
2025-09-01 10:04:19 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-#exec socat TCP-LISTEN:22,fork TCP:$TARGET_HOST:22
-exec wstunnel server --log-lvl DEBUG wss://[::]:443
+# Set the default protocol and port if not provided
+if [ -z "$INCOMING_WS_PORT" ]; then
+    INCOMING_WS_PORT=443
+    echo "Using default port: $INCOMING_WS_PORT"
+else
+    echo "Using port: $INCOMING_WS_PORT"
+fi
+
+if [ "$INCOMING_WS_TLS_ENABLED" = "false" ]; then
+    PROTOCOL="ws"
+    echo "TLS is disabled, using protocol: $PROTOCOL"
+else
+    PROTOCOL="wss"
+    echo "TLS is enabled, using protocol: $PROTOCOL"
+fi
+
+exec wstunnel server --log-lvl DEBUG ${PROTOCOL}://[::]:${INCOMING_WS_PORT}
